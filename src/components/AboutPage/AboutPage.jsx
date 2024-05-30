ef15ab4a5e82bea42011badbf255c05a63a0a5cb
Mustafe Hassan <mustafeh123@gmail.com>
2024-05-30 15:14:47 -0500

    Adding text to the about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,7 +9,20 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p>Hello to the User's reading this!</p>
+
+        <p>My name is Mustafe Hassan and I am the person who made the web app 
+           you're currently on! This webapp uses Front-end and Back-end, for the Front-end technologies 
+           I used were HTML, CSS, Javascript, React/Redux, for the Back-end Technologies I used Node.js, 
+           PostgreSQL, Express.js </p>
+        <p>
+          The reason I built this webapp is that I am currently in the field working in healthcare, I specifically 
+          work in group homes where I aid the daily life of a person I support. As someone who administers medication
+          it is the upmost importance to make sure the person I support get's their medication on time. There is only
+          a small margin to where they can miss their medication and thats only 30 minutes, once it's passed the 30 minute
+          window the medication is not administerable anymore.This Web app can help combat any possiblity of being late for 
+          either your own medication or a person you are administering medicaiton to!
+        </p>
       </div>
     </div>
   );
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -6,7 +6,7 @@ import './LandingPage.css';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+  const [heading, setHeading] = useState('Welcome to Mustafes Medication Reminder Web App!');
   const history = useHistory();
 
   const onLogin = (event) => {
@@ -15,41 +15,18 @@ function LandingPage() {
 
   return (
     <div className="container">
-      <h2>{heading}</h2>
+      <h1>{heading}</h1>
 
       <div className="grid">
         <div className="grid-col grid-col_8">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
-          </p>
-
-          <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
-          </p>
-
-          <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
-          </p>
+      
+          <h2>
+            To get Started :
+          </h2>
+           <ul>
+            <li>Please create a Username & Password you can remember, There isn't any current password and username retrival system yet ! </li>
+            <li>Make sure to add the correct information as it is not editable !</li>
+           </ul>
         </div>
         <div className="grid-col grid-col_4">
           <RegisterForm />
